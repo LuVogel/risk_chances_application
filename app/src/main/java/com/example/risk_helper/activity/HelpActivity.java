5f5de1019082da24005f99cff246b04099449424
LuVogel <lukas.vogel@stud.unibas.ch>
2021-12-06 18:32:24 +0100

    helper function

---
+++ app/src/main/java/com/example/risk_helper/activity/HelpActivity.java
@@ -0,0 +1,18 @@
+package com.example.risk_helper.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.risk_helper.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+
+    }
+}
--- app/src/main/java/com/example/risk_helper/activity/MainActivity.java
+++ app/src/main/java/com/example/risk_helper/activity/MainActivity.java
@@ -5,28 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.risk_helper.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView welcomeScreenTextView;
-    ImageButton welcomeNextScreenButton;
+    TextView welcomeContinueTextViewAsButton;
+    TextView welcomeHelpTextViewAsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        welcomeScreenTextView = findViewById(R.id.welcome_textView);
-        welcomeNextScreenButton = findViewById(R.id.welcome_next_WindowButton);
+        welcomeContinueTextViewAsButton = findViewById(R.id.welcome_continue_textView);
+        welcomeHelpTextViewAsButton = findViewById(R.id.welcome_help_textView);
 
-        welcomeNextScreenButton.setOnClickListener(new View.OnClickListener() {
+        welcomeContinueTextViewAsButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, InputFieldActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        welcomeHelpTextViewAsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class);
                 startActivity(intent);
             }
         });
